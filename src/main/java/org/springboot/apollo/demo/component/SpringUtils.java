02066d3962ed9d4ae9408a5e6abbc57a39b76775
Wq <1144502582@qq.com>
2020-12-27 08:57:58 +0800

    springboot工具类

---
+++ src/main/java/org/springboot/apollo/demo/component/SpringUtils.java
@@ -0,0 +1,37 @@
+package org.springboot.apollo.demo.component;
+
+import lombok.Getter;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author： WangQian
+ * @date： 2020/7/29 下午 6:31
+ */
+@Component
+public final class SpringUtils implements ApplicationContextAware {
+
+    @Getter
+    private static ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        if (SpringUtils.applicationContext == null) {
+            SpringUtils.applicationContext = applicationContext;
+        }
+    }
+
+    public static <T> T getBean(Class<T> clazz) {
+        return SpringUtils.applicationContext.getBean(clazz);
+    }
+
+    public static Object getBean(String name) {
+        return SpringUtils.applicationContext.getBean(name);
+    }
+
+    public static String getProperty(String key) {
+        return SpringUtils.applicationContext.getEnvironment().getProperty(key);
+    }
+}
