cb23e1dacc0cf896479e2e4e51da0a5e77663fba
Wq <1144502582@qq.com>
2020-12-30 13:51:21 +0800

    应用配置类

---
+++ src/main/java/org/springboot/apollo/demo/configuration/apollo/AppConfig.java
@@ -0,0 +1,14 @@
+package org.springboot.apollo.demo.configuration.apollo;
+
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author： WangQian
+ * @date： 2020/8/12 下午 4:44
+ */
+
+@Configuration
+@EnableApolloConfig
+public class AppConfig {
+}
